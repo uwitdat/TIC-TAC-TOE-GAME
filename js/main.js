325c5ff07cdd1d89fb2ca0ba4f7bbc00e69d0610
Ben Shekhtman <benshekhtman@hotmail.com>
2021-04-14 18:41:42 -0400

    functionality updates

--- js/main.js
+++ js/main.js
@@ -4,18 +4,26 @@ let gameText = document.querySelector('.game-text')
 let playAgain = document.querySelector('.play-again')
 
 gameState = 0
+
 gameScore = {
     x: 0,
     o: 0,
+    ties: 0,
 }
 
 let x = 'X'
 let o = 'O'
 
 let scoreX = document.querySelector('.x')
-scoreX.innerHTML = `Xs: ${gameScore.x}`
+scoreX.innerHTML = `Xs: ${gameScore.x}|`
 let scoreO = document.querySelector('.o')
-scoreO.innerHTML = `Os: ${gameScore.o}`
+scoreO.innerHTML = `Os: ${gameScore.o}|`
+let scoreTies = document.querySelector('.ties')
+scoreTies.innerHTML = `Ties: ${gameScore.ties}|`
+
+playAgain.addEventListener('click', function (e){
+    reset()
+})
 
 //DOMS
 btnsDiv.addEventListener('click',function (e){
@@ -40,13 +48,167 @@ btnsDiv.addEventListener('click',function (e){
 
 function winner(){
     if(btnsDiv.children[0].innerText === x && btnsDiv.children[1].innerText === x && btnsDiv.children[2].innerText === x){
-        btnsDiv.children.disabled = true
         btnsDiv.children[0].style.color = 'green';
         btnsDiv.children[1].style.color = 'green';
         btnsDiv.children[2].style.color = 'green';
         gameText.innerHTML = 'Xs WIN!'
         gameScore.x ++
+        disableButtons();
         scoreX.innerHTML = `Xs: ${gameScore.x}`
         playAgain.style.display = 'block';
+    }   if(btnsDiv.children[0].innerText === o && btnsDiv.children[1].innerText === o && btnsDiv.children[2].innerText === o){
+        btnsDiv.children[0].style.color = 'green';
+        btnsDiv.children[1].style.color = 'green';
+        btnsDiv.children[2].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[3].innerText === x && btnsDiv.children[4].innerText === x && btnsDiv.children[5].innerText === x){
+        btnsDiv.children[3].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[5].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[3].innerText === o && btnsDiv.children[4].innerText === o && btnsDiv.children[5].innerText === o){
+        btnsDiv.children[3].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[5].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[6].innerText === x && btnsDiv.children[7].innerText === x && btnsDiv.children[8].innerText === x){
+        btnsDiv.children[6].style.color = 'green';
+        btnsDiv.children[7].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[6].innerText === o && btnsDiv.children[7].innerText === o && btnsDiv.children[8].innerText === o){
+        btnsDiv.children[6].style.color = 'green';
+        btnsDiv.children[7].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[0].innerText === x && btnsDiv.children[3].innerText === x && btnsDiv.children[6].innerText === x){
+        btnsDiv.children[0].style.color = 'green';
+        btnsDiv.children[3].style.color = 'green';
+        btnsDiv.children[6].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[0].innerText === o && btnsDiv.children[3].innerText === o && btnsDiv.children[6].innerText === o){
+        btnsDiv.children[0].style.color = 'green';
+        btnsDiv.children[3].style.color = 'green';
+        btnsDiv.children[6].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[1].innerText === x && btnsDiv.children[4].innerText === x && btnsDiv.children[7].innerText === x){
+        btnsDiv.children[1].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[7].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[1].innerText === o && btnsDiv.children[4].innerText === o && btnsDiv.children[7].innerText === o){
+        btnsDiv.children[1].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[7].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[2].innerText === x && btnsDiv.children[5].innerText === x && btnsDiv.children[8].innerText === x){
+        btnsDiv.children[2].style.color = 'green';
+        btnsDiv.children[5].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[2].innerText === o && btnsDiv.children[5].innerText === o && btnsDiv.children[8].innerText === o){
+        btnsDiv.children[2].style.color = 'green';
+        btnsDiv.children[5].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[0].innerText === x && btnsDiv.children[4].innerText === x && btnsDiv.children[8].innerText === x){
+        btnsDiv.children[0].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[0].innerText === o && btnsDiv.children[4].innerText === o && btnsDiv.children[8].innerText === o){
+        btnsDiv.children[0].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[8].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[2].innerText === x && btnsDiv.children[4].innerText === x && btnsDiv.children[6].innerText === x){
+        btnsDiv.children[2].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[6].style.color = 'green';
+        gameText.innerHTML = 'Xs WIN!'
+        gameScore.x ++
+        disableButtons();
+        scoreX.innerHTML = `Xs: ${gameScore.x}`
+        playAgain.style.display = 'block';
+    }   if(btnsDiv.children[2].innerText === o && btnsDiv.children[4].innerText === o && btnsDiv.children[6].innerText === o){
+        btnsDiv.children[2].style.color = 'green';
+        btnsDiv.children[4].style.color = 'green';
+        btnsDiv.children[6].style.color = 'green';
+        gameText.innerHTML = 'Os WIN!'
+        gameScore.o ++
+        disableButtons();
+        scoreO.innerHTML = `Os: ${gameScore.o}`
+        playAgain.style.display = 'block';
+    }   
+}
+
+
+function disableButtons(){
+    for(let btn of btnsDiv.children){
+        btn.disabled = true
+        if(btn.innerText === 'null'){
+            btn.style.color = 'white';
+        }
+    }
+}
+
+function reset(){
+    for(let btn of btnsDiv.children){
+        btn.removeAttribute('disabled');
+        btn.style.color = 'white';
+        btn.innerText = '1'
+        gameText.innerHTML = 'Play Again!'
     }
 }
