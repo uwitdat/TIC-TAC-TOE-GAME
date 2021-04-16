488ccea9057272ca702853e1851478c59aee75aa
Ben Shekhtman <benshekhtman@hotmail.com>
2021-04-15 20:54:53 -0400

    styling started

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 //VARIABLES
-let btnsDiv = document.querySelector('.buttons');
-let gameText = document.querySelector('.game-text')
-let playAgain = document.querySelector('.play-again')
-
 gameState = 0
 
 gameScore = {
@@ -11,39 +7,47 @@ gameScore = {
     ties: 0,
 }
 
+round = 1
+
 let x = 'X'
 let o = 'O'
 
+let btnsDiv = document.querySelector('.buttons');
+let gameText = document.querySelector('.game-text')
+let playAgain = document.querySelector('.play-again')
 let scoreX = document.querySelector('.x')
-scoreX.innerHTML = `Xs: ${gameScore.x}`
 let scoreO = document.querySelector('.o')
-scoreO.innerHTML = `Os: ${gameScore.o}`
 let scoreTies = document.querySelector('.ties')
-scoreTies.innerHTML = `Ties: ${gameScore.ties}`
 
-playAgain.addEventListener('click', function (e){
-    reset()
-})
+let roundText = document.querySelector('.round')
+roundText.textContent = `Round: ${round}`
 
 //DOMS
+playAgain.addEventListener('click', function (e){
+    reset()
+    round ++
+    roundText.textContent = `Round: ${round}`
+    playAgain.style.display = 'none'
+})
+
 btnsDiv.addEventListener('click',function (e){
     if(e.target.tagName !== 'BUTTON') return;
+
         if(gameState % 2 === 0){   
         e.target.innerText = x
         e.target.disabled = true
         e.target.style.color = 'blue'
         gameState++
-        console.log(gameState)
         gameText.textContent = 'Os TURN.'
         winner();
         ties();
 
         } else {
+
         e.target.innerText= o
         e.target.disabled = true
         e.target.style.color = 'red'
         gameState++
-        console.log(gameState)
         gameText.textContent = 'Xs TURN.'
         winner();     
         ties();
@@ -51,6 +55,7 @@ btnsDiv.addEventListener('click',function (e){
     }
 )
 
+// ALL POSSIBLE INDEX OUTCOMES FOR WINNING CONDITION
 outcomeLines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -69,13 +74,24 @@ for (line of outcomeLines) {
         btnsDiv.children[a].style.color = 'green';
         btnsDiv.children[b].style.color = 'green';
         btnsDiv.children[c].style.color = 'green';
+        renderGrey();
         xWin();
+
     }if(btnsDiv.children[a].innerText === o && btnsDiv.children[b].innerText === o && btnsDiv.children [c].innerText === o){
         btnsDiv.children[a].style.color = 'green';
         btnsDiv.children[b].style.color = 'green';
         btnsDiv.children[c].style.color = 'green';
+        renderGrey();
         oWin();   
         } 
+    }
+}
+
+function renderGrey(){
+    for(let btn of btnsDiv.children){
+        if(btn.style.color != 'green'){
+            btn.style.color = 'grey';
+        }
     }
 }
 
@@ -93,7 +109,7 @@ function reset(){
         btn.removeAttribute('disabled');
         btn.style.color = 'white';
         btn.innerText = 'null'
-        gameText.innerHTML = 'Play Again!'
+
         gameState = 0
     }
 }
